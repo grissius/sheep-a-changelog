33ebc313cf7795b6161719e800c75d3d72105054
Jaroslav Šmolík <grissius@gmail.com>
2019-01-10 17:08:15 +0100

    wip

--- lib/sheep-a-changelog/node.rb
+++ lib/sheep-a-changelog/node.rb
@@ -5,44 +5,40 @@ module SheepAChangelog
       new(string.split("\n"))
     end
 
+    def hashes(n = @level)
+      '#' * n
+    end
+
     # Contruct nodes for current node from all lines
-    def build_nodes(lines, level)
-      last = :self
+    def build_nodes(lines, next_level)
+      last = nil
       nodes = []
       line_buff = []
       lines.map do |line|
-        match = line.match(Regexp.new('^' + ('#' * (level + 1)) + '\s+([\S\s]+)$'))
+        match = line.match(Regexp.new('^' + hashes(next_level) + '\s+([\S\s]+)$'))
         heading = match && match.captures.first
         if heading
-          if last.is_a?(Symbol)
-            @lines = line_buff.clone
-          else
-            nodes << Node.new(line_buff.clone, last, level + 1)
-          end
+          @lines, nodes = last.nil? ? [line_buff, nodes] : [@lines, nodes << Node.new(line_buff, last, next_level)]
           last = heading
           line_buff = []
         else
           line_buff << line
         end
       end
-      if last.is_a?(Symbol)
-        @lines = line_buff.clone
-      else
-        nodes << Node.new(line_buff.clone, last, level + 1)
-      end
+      @lines, nodes = last.nil? ? [line_buff, nodes] : [@lines, nodes << Node.new(line_buff, last, next_level)]
       nodes
     end
 
     # Create node hierarchy from keep-a-changeloh markdown lines
     def initialize(lines, title = :empty, level = 0)
       @title = title
-      @nodes = build_nodes(lines, level)
+      @nodes = build_nodes(lines, level + 1)
       @level = level
     end
 
     # Format your node's title
     def format_heading
-      "#{'#' * @level} #{@title}"
+      "#{hashes} #{@title}"
     end
 
     # Get lines from
