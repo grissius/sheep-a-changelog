968c8f20002f688806639efc12c2bfba8c72ef73
Jaroslav Šmolík <grissius@gmail.com>
2019-01-14 18:16:35 +0100

    Remove version from meta snapshot test

--- spec/sheep-a-changelog/meta_spec.rb
+++ spec/sheep-a-changelog/meta_spec.rb
@@ -2,7 +2,7 @@ require 'sheep-a-changelog/meta'
 
 RSpec.describe 'Meta data' do
   it 'works and is unchanged' do
-    meta = %i[version author name description email license homepage summary]
+    meta = %i[author name description email license homepage summary]
            .map { |m| [m, SheepAChangelog::Meta.public_send(m)] }.to_h
     expect(meta).to match_snapshot
   end
