6ccccfcda1d634753e89d9b031efc5a92cac8d14
Jaroslav Šmolík <grissius@gmail.com>
2019-05-06 07:29:16 +0200

    Limit integration snapshot

--- spec/sheep-a-changelog/repo_inspector_spec.rb
+++ spec/sheep-a-changelog/repo_inspector_spec.rb
@@ -5,6 +5,14 @@ RSpec.describe SheepAChangelog::RepoInspector do
     root_path = File.expand_path('../..', __dir__)
     inspector = SheepAChangelog::RepoInspector.new(root_path)
     doc = inspector.init_changelog
+    # removed unreleased
+    nodes = doc.nodes.first.nodes
+    doc.nodes.first.nodes = doc.nodes.first.nodes.reject do |n|
+      tag, date_str = n.title.split(' - ')
+      bail = Date::strptime(date_str, '%Y-%m-%d') > Date.new(2019, 02, 16)
+      # TODO: use tag and bail to reject anchors
+      bail
+    end
     expect(doc.to_s).to match_snapshot
   end
 end
